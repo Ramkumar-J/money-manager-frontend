85ec9d02f362b3251d663515f075a5be488191ed
ramkumar <ramviru9@gmail.com>
2022-06-05 15:48:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import DailyPlanform from "./DailyPlanform";
-import WeeklyPlanform from "./WeeklyPlanform";
 import MonthlyPlanform from "./MonthlyPlanform";
 import YearlyPlanform from "./YearlyPlanform";
+import Plans from "./Plans";
 import DailyPlans from "./DailyPlans";
-import WeeklyPlans from "./WeeklyPlans";
 import MonthlyPlans from "./MonthlyPlans";
 import YearlyPlans from "./YearlyPlans";
 import AddExpenses from "./AddExpenses";
 import AddIncomes from "./AddIncomes";
 import DailyPlanView from "./DailyPlanView";
-import Plans from "./Plans";
 
 function App() {
   return (
@@ -29,16 +27,14 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/Home" element={<Home />} />
             <Route path="/DailyPlanform" element={<DailyPlanform />} />
-            <Route path="/WeeklyPlanform" element={<WeeklyPlanform />} />
             <Route path="/MonthlyPlanform" element={<MonthlyPlanform />} />
             <Route path="/YearlyPlanform" element={<YearlyPlanform />} />
+            <Route path="/Plans" element={<Plans />} />
             <Route path="/DailyPlans" element={<DailyPlans />} />
-            <Route path="/WeeklyPlans" element={<WeeklyPlans />} />
             <Route path="/MonthlyPlans" element={<MonthlyPlans />} />
             <Route path="/YearlyPlans" element={<YearlyPlans />} />
             <Route path="/AddExpenses" element={<AddExpenses />} />
             <Route path="/AddIncomes" element={<AddIncomes />} />
-            <Route path="/Plans" element={<Plans />} />
             <Route path="/DailyPlanView" element={<DailyPlanView />} />
             <Route path="/DailyPlanView/:id" element={<DailyPlanView />} />
           </Routes>
